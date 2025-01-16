9d6a42aca06d95ba11c830bd830d56c8dd6b51f4
221B433 <221B433@juet.co.in>
2025-01-16 14:58:13 +0530

    LE0

---
+++ LE0.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+// Java Program to print the multiplication table of the number N using for loop
+
+class Varsha {
+    public static void main(String[] args)
+    {
+        int N = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " * " + i + " = " + N * i);
+        }
+    }
+}
